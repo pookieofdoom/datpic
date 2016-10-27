f0dcf14ab311640447d3b2032f952e63c2865263
Anthony Dinh <pookieofdoom@gmail.com>
2016-10-26 22:24:27 -0700

    started fragment stuff

---
+++ app/src/main/java/adinh03/calpoly/edu/datpic/EntryFragment.java
@@ -0,0 +1,21 @@
+package adinh03.calpoly.edu.datpic;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Anthony on 10/26/16.
+ */
+
+public class EntryFragment extends Fragment {
+
+   @Nullable
+   @Override
+   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+      return super.onCreateView(inflater, container, savedInstanceState);
+   }
+}
