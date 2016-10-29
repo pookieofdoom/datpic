a0dbe57d4288e3cb30b1899fb64917aae93ee913
tjyung <Kodajyung1>
2016-10-29 16:16:37 -0700

    adding a user

--- app/src/main/java/adinh03/calpoly/edu/datpic/MainActivity.java
+++ app/src/main/java/adinh03/calpoly/edu/datpic/MainActivity.java
@@ -1,13 +1,36 @@
 package adinh03.calpoly.edu.datpic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-   @Override
+public class MainActivity extends AppCompatActivity{
+   private FirebaseAuth mFirebaseAuth;
+   private FirebaseUser mFirebaseUser;
+
+
+    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
+
+       // Initialize Firebase Auth
+       mFirebaseAuth = FirebaseAuth.getInstance();
+       mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
+       if (mFirebaseUser == null) {
+           // Not logged in, launch the Log In activity
+           loadLogInView();
+       }
+   }
+
+   private void loadLogInView() {
+      Intent intent = new Intent(this, LogInActivity.class);
+      intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+      intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+      startActivity(intent);
    }
 }
