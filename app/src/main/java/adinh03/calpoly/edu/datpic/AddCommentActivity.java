98394300c719992258c236be5cb2340893ad27e9
Anthony Dinh <pookieofdoom@gmail.com>
2016-11-06 17:43:45 -0800

    Restructured the database layout

--- app/src/main/java/adinh03/calpoly/edu/datpic/AddCommentActivity.java
+++ app/src/main/java/adinh03/calpoly/edu/datpic/AddCommentActivity.java
@@ -1,8 +1,6 @@
 package adinh03.calpoly.edu.datpic;
 
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -23,191 +21,223 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
-
-import org.w3c.dom.Comment;
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 /**
  * Created by Dylan on 10/30/16.
  */
 
-public class AddCommentActivity extends AppCompatActivity {
-    EditText inputComment;
-    Button submitCommentButton;
-    ArrayList<CommentEntry> commentList;
-    RecyclerView commentRecyclerView;
-    private CommentAdapter adapter;
-    DatabaseReference ref = FirebaseDatabase.getInstance().getReference("users");
-    private FirebaseDatabase mDataBase;
-    private FirebaseUser mFirebaseUser;
-    private FirebaseAuth mFirebaseAuth;
+public class AddCommentActivity extends AppCompatActivity
+{
+   EditText inputComment;
+   Button submitCommentButton;
+   ArrayList<CommentEntry> commentList;
+   RecyclerView commentRecyclerView;
+   private CommentAdapter adapter;
+   DatabaseReference mRef = FirebaseDatabase.getInstance().getReference("users");
+   private FirebaseDatabase mDataBase;
+   private FirebaseUser mFirebaseUser;
+   private FirebaseAuth mFirebaseAuth;
+   private int clickedImageIndex;
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState)
+   {
+      super.onCreate(savedInstanceState);
+
+      setContentView(R.layout.comment_activity);
+
+      commentList = (ArrayList<CommentEntry>) getLastCustomNonConfigurationInstance();
+      if (commentList == null)
+      {
+         commentList = new ArrayList<>();
+      }
+      clickedImageIndex = getIntent().getIntExtra("clickedImageIndex", -1);
+      inputComment = (EditText) findViewById(R.id.InputCommentTextBox);
+      submitCommentButton = (Button) findViewById(R.id.CommentSubmitButton);
+      commentRecyclerView = (RecyclerView) findViewById(R.id.CommentRecylerView);
+
+      commentRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager
+            .VERTICAL, false));
+
+      mFirebaseAuth = FirebaseAuth.getInstance();
+      mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
+      mRef.addValueEventListener(populateCommentListener());
+      //after update the list
+      adapter = new CommentAdapter(commentList);
+
+      commentRecyclerView.setAdapter(adapter);
+
+      inputComment.setOnKeyListener(new View.OnKeyListener()
+      {
+         @Override
+         public boolean onKey(View view, int keyCode, KeyEvent keyEvent)
+         {
+            if (keyEvent.getAction() == KeyEvent.ACTION_DOWN)
+            {
+               System.out.println("Before" + inputComment.getText().toString());
+
+               if ((keyCode == KeyEvent.KEYCODE_ENTER) && isEmptyString(inputComment))
+               {
+                  addComment(inputComment, clickedImageIndex);
+                  //adds comment in database for photo
+                  inputComment.setText(null);
+               }
+            }
+            return false;
+         }
+      });
+
+      submitCommentButton.setOnClickListener(new View.OnClickListener()
+      {
+         @Override
+         public void onClick(View view)
+         {
+            if (isEmptyString(inputComment))
+            {
+               addComment(inputComment, clickedImageIndex);
+               inputComment.setText(null);
+            }
+         }
+      });
+
+   }
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+   protected void addComment(EditText text, int clickedImageIndex)
+   {
 
-        setContentView(R.layout.comment_activity);
+      String path = StaticEntryList.getInstance().getMap().get(StaticEntryList.getInstance()
+            .getEntry(clickedImageIndex).getUri().toString());
+      DatabaseReference ref = mRef.child(mFirebaseUser.getUid()).child("Entry");
+      String commentId = ref.push().getKey();
+      ref.child(commentId).child("comment").setValue(text.getText().toString());
+      ref.child(commentId).child("imageLoc").setValue(path);
+      CommentEntry newEntry = new CommentEntry();
+      newEntry.setText(text.getText().toString());
+      commentList.add(newEntry);
+      adapter.notifyDataSetChanged();
+   }
 
-        commentList = (ArrayList<CommentEntry>) getLastCustomNonConfigurationInstance();
-        if (commentList == null) {
-            commentList = new ArrayList<>();
-        }
+   private ValueEventListener populateCommentListener()
+   {
+      return new ValueEventListener()
+      {
+         @Override
+         public void onDataChange(DataSnapshot dataSnapshot)
+         {
+            commentList.clear();
+            for (DataSnapshot userSnapshot : dataSnapshot.getChildren())
+            {
+               Log.d("DEBUG2", "User is: " + userSnapshot.getKey());
+               for (DataSnapshot entrySnapShot : userSnapshot.getChildren())
+               {
+                  if (entrySnapShot.getKey().equals("Entry"))
+                  {
+                     for (DataSnapshot commentSnapShot : entrySnapShot.getChildren())
+                     {
+                        Log.d("DEBUG2", "CommentId is " + commentSnapShot.getKey().toString());
+                        if (commentSnapShot.child("imageLoc").getValue() != null &&
+                              commentSnapShot.child("imageLoc").getValue(String.class).equals
+                                    (StaticEntryList.getInstance()
+                                          .getMap
+                                                ().get(StaticEntryList
+                                                .getInstance().getEntry(clickedImageIndex).getUri()
+                                                .toString
+                                                      ())))
+                        {
 
-        inputComment = (EditText) findViewById(R.id.InputCommentTextBox);
-        submitCommentButton = (Button) findViewById(R.id.CommentSubmitButton);
-        commentRecyclerView = (RecyclerView) findViewById(R.id.CommentRecylerView);
+                           CommentEntry insert = new CommentEntry();
+                           insert.setText(commentSnapShot.child("comment").getValue(String.class));
+                           if (!commentList.contains(insert))
+                           {
+                              commentList.add(insert);
+                              adapter.notifyDataSetChanged();
+                           }
 
-        commentRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        mFirebaseUser = mFirebaseAuth.getCurrentUser();
-        final int clickedImageIndex = getIntent().getIntExtra("clickedImageIndex", -1);
-
-
-
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("users");
-
-        ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                commentList.clear();
-                for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
-                    Log.d("DEBUG", "User is: " + userSnapshot.getKey());
-                    for (DataSnapshot imageShots : userSnapshot.getChildren()) {
-                        Log.d("DEBUG", "Image is: " + imageShots.getKey());
-
-                        if (imageShots.getKey().toString().equals(StaticEntryList.getInstance().getMap().get(StaticEntryList
-                              .getInstance().getEntry(clickedImageIndex).getUri().toString()))) {
-
-
-                            for (DataSnapshot commentShots : imageShots.child("comments").getChildren()) {
-                                CommentEntry insert = new CommentEntry();
-                                insert.setText(commentShots.getValue().toString());
-//                                commentList.clear();
-                                if (!commentList.contains(insert)) {
-                                    commentList.add(insert);
-                                }
-                                adapter.notifyDataSetChanged();
-
-                            }
-                            populateAllComments();
                         }
-                    }
-
-                }
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
+                     }
+                  }
+               }
 
             }
-        });
-        //after update the list
-        adapter = new CommentAdapter(commentList);
-        System.out.println("adapter: " + adapter.mList.size());
-        commentRecyclerView.setAdapter(adapter);
+
+         }
+
+         @Override
+         public void onCancelled(DatabaseError databaseError)
+         {
+
+         }
+      };
+   }
+
+   protected boolean isEmptyString(EditText text)
+   {
+      String input = text.getText().toString();
+      return input.trim().length() != 0 && !TextUtils.isEmpty(input);
+   }
 
 
-        inputComment.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
-                if (keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
-                    System.out.println("Before" + inputComment.getText().toString());
+   @Override
+   protected void onRestoreInstanceState(Bundle savedInstanceState)
+   {
+      super.onRestoreInstanceState(savedInstanceState);
+   }
 
-                    if ((keyCode == KeyEvent.KEYCODE_ENTER) && isEmptyString(inputComment)) {
-                        System.out.println(inputComment.getText().toString());
-                        addComment(inputComment, clickedImageIndex);
-                        //adds comment in database for photo
-                        inputComment.setText(null);
-                    }
-                }
-                return false;
-            }
-        });
+   public class CommentAdapter extends RecyclerView.Adapter<CommentHolder>
+   {
+      private ArrayList<CommentEntry> mList;
 
-        submitCommentButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (isEmptyString(inputComment)) {
-                    addComment(inputComment, clickedImageIndex);
-                    inputComment.setText(null);
-                }
-            }
-        });
+      public CommentAdapter(ArrayList<CommentEntry> list)
+      {
+         mList = list;
+      }
 
-    }
+      @Override
+      public CommentHolder onCreateViewHolder(ViewGroup parent, int viewType)
+      {
+         return new CommentHolder(LayoutInflater.from(parent.getContext()).inflate(viewType,
+               parent, false));
 
-    protected void populateAllComments() {
+      }
 
-    }
+      @Override
+      public int getItemViewType(int position)
+      {
+         return R.layout.comment_entry;
+      }
 
-    protected void addComment(EditText text, int clickedImageIndex) {
+      @Override
+      public void onBindViewHolder(CommentHolder holder, int position)
+      {
+         holder.bind(mList.get(position));
+      }
 
-        String path = StaticEntryList.getInstance().getMap().get(StaticEntryList.getInstance().getEntry(clickedImageIndex).getUri().toString());
-        mDataBase.getInstance().getReference("users").child(mFirebaseUser.getUid()).child(path).child("comments").push().setValue(text.getText().toString());
-        CommentEntry newEntry = new CommentEntry();
-        newEntry.setText(text.getText().toString());
-        commentList.add(newEntry);
-        adapter.notifyDataSetChanged();
-    }
+      @Override
+      public int getItemCount()
+      {
+         return mList.size();
+      }
+   }
 
-    protected boolean isEmptyString(EditText text) {
-        String input = text.getText().toString();
-        return input.trim().length() != 0 && !TextUtils.isEmpty(input);
-    }
+   public static class CommentHolder extends RecyclerView.ViewHolder
+   {
+      private TextView commentTextView;
 
+      public CommentHolder(View view)
+      {
+         super(view);
+         commentTextView = (TextView) view.findViewById(R.id.commentView);
+      }
 
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState){
-        super.onRestoreInstanceState(savedInstanceState);
-    }
-
-    public class CommentAdapter extends RecyclerView.Adapter<CommentHolder> {
-        private ArrayList<CommentEntry> mList;
-        public CommentAdapter(ArrayList<CommentEntry> list) {
-            mList = list;
-        }
-
-        @Override
-        public CommentHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return new CommentHolder(LayoutInflater.from(parent.getContext()).inflate(viewType, parent, false));
-
-        }
-
-        @Override
-        public int getItemViewType(int position) {
-            return R.layout.comment_entry;
-        }
-
-        @Override
-        public void onBindViewHolder(CommentHolder holder, int position) {
-            holder.bind(mList.get(position));
-        }
-
-        @Override
-        public int getItemCount() {
-            return mList.size();
-        }
-    }
-
-    public static class CommentHolder extends RecyclerView.ViewHolder {
-        private TextView commentTextView;
-        public CommentHolder(View view) {
-            super(view);
-            commentTextView = (TextView) view.findViewById(R.id.commentView);
-        }
-
-        public void bind(final CommentEntry commentEntry) {
-            commentTextView.setText(commentEntry.getText());
-        }
+      public void bind(final CommentEntry commentEntry)
+      {
+         commentTextView.setText(commentEntry.getText());
+      }
 
 
-    }
+   }
 }
--- app/src/main/java/adinh03/calpoly/edu/datpic/MainActivity.java
+++ app/src/main/java/adinh03/calpoly/edu/datpic/MainActivity.java
@@ -3,7 +3,6 @@ package adinh03.calpoly.edu.datpic;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -25,9 +24,9 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity
+{
    private FirebaseAuth mFirebaseAuth;
    private FirebaseUser mFirebaseUser;
    private RecyclerView mRecyclerView;
@@ -41,102 +40,122 @@ public class MainActivity extends AppCompatActivity{
    private static final int TEST_UPLOAD_INTENT = 2;
 
 
-    @Override
-   protected void onCreate(Bundle savedInstanceState) {
+   @Override
+   protected void onCreate(Bundle savedInstanceState)
+   {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
 
-       // Initialize Firebase Auth
-       mFirebaseAuth = FirebaseAuth.getInstance();
-       mFirebaseUser = mFirebaseAuth.getCurrentUser();
+      // Initialize Firebase Auth
+      mFirebaseAuth = FirebaseAuth.getInstance();
+      mFirebaseUser = mFirebaseAuth.getCurrentUser();
 
-       if (mFirebaseUser == null) {
-          // Not logged in, launch the Log In activity
-          loadLogInView();
-       }
+      if (mFirebaseUser == null)
+      {
+         // Not logged in, launch the Log In activity
+         loadLogInView();
+      }
 
-       // Initializing Entry Objects
-       mEntryList = (ArrayList<Entry>) getLastCustomNonConfigurationInstance();
-       if (mEntryList == null) {
-          mEntryList = new ArrayList<>();
-       }
-       StaticEntryList.getInstance().setEntry(mEntryList);
+      // Initializing Entry Objects
+      mEntryList = (ArrayList<Entry>) getLastCustomNonConfigurationInstance();
+      if (mEntryList == null)
+      {
+         mEntryList = new ArrayList<>();
+      }
+      StaticEntryList.getInstance().setEntry(mEntryList);
 
-       mAdapter = new MyAdapter(mEntryList);
+      mAdapter = new MyAdapter(mEntryList);
 
-       // Initializing Recycler View
-       mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-       mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-       //these lines of code fixed stuttering when scrolling images
-       mRecyclerView.setHasFixedSize(true);
-       mRecyclerView.setItemViewCacheSize(20);
-       mRecyclerView.setDrawingCacheEnabled(true);
-       mRecyclerView.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
+      // Initializing Recycler View
+      mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+      mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,
+            false));
+      //these lines of code fixed stuttering when scrolling images
+      mRecyclerView.setHasFixedSize(true);
+      mRecyclerView.setItemViewCacheSize(20);
+      mRecyclerView.setDrawingCacheEnabled(true);
+      mRecyclerView.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
 
-       mRecyclerView.setAdapter(mAdapter);
+      mRecyclerView.setAdapter(mAdapter);
 
-       // Initialize Storage
-       mStorage = FirebaseStorage.getInstance();
+      // Initialize Storage
+      mStorage = FirebaseStorage.getInstance();
 
 
-       //Test for uploading
-       mTestUpload = (Button) findViewById(R.id.testUpload);
-       mTestUpload.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-             Intent imageIntent = new Intent(Intent.ACTION_GET_CONTENT);
-             imageIntent.setType("image/*");
-             startActivityForResult(imageIntent, TEST_UPLOAD_INTENT);
+      //Test for uploading
+      mTestUpload = (Button) findViewById(R.id.testUpload);
+      mTestUpload.setOnClickListener(new View.OnClickListener()
+      {
+         @Override
+         public void onClick(View v)
+         {
+            Intent imageIntent = new Intent(Intent.ACTION_GET_CONTENT);
+            imageIntent.setType("image/*");
+            startActivityForResult(imageIntent, TEST_UPLOAD_INTENT);
 
-          }
-       });
+         }
+      });
 
-       //DatabaseReference dbRef = mDataBase.getReference();
+      //DatabaseReference dbRef = mDataBase.getReference();
       //Test for downloading all images (only for Vertical Prototype)
-       populateAllImages();
+      populateAllImages();
 
 
    }
 
-   private void populateAllImages() {
+   private void populateAllImages()
+   {
 
       DatabaseReference ref = FirebaseDatabase.getInstance().getReference("users");
-      ref.addValueEventListener(new ValueEventListener() {
+      ref.addValueEventListener(new ValueEventListener()
+      {
          @Override
-         public void onDataChange(DataSnapshot dataSnapshot) {
+         public void onDataChange(DataSnapshot dataSnapshot)
+         {
             mEntryList.clear();
+            boolean flag = false;
 
-            for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+            for (DataSnapshot userSnapshot : dataSnapshot.getChildren())
+            {
                Log.d("DEBUG", "User is: " + userSnapshot.getKey());
-               for (DataSnapshot imageShots : userSnapshot.getChildren()) {
+               for (DataSnapshot imageShots : userSnapshot.getChildren())
+               {
                   Log.d("DEBUG", "Image is: " + imageShots.getKey());
-
-                     Uri data = Uri.parse(imageShots.child("url").getValue().toString());
-                     Entry insert = new Entry(0, 0, data);
-                     if (!mEntryList.contains(insert)) {
-                        mEntryList.add(insert);
-                        mAdapter.notifyDataSetChanged();
-                        StaticEntryList.getInstance().setMap(data.toString(), imageShots.getKey());
+                  if (imageShots.getKey().equals("Images"))
+                  {
+                     for (DataSnapshot urlShots : imageShots.getChildren())
+                     {
+                        Log.d("DEBUG", "Url is " + urlShots.getValue(String.class));
+                        Uri data = Uri.parse(urlShots.getValue().toString());
+                        Entry insert = new Entry(0, 0, data);
+                        if (!mEntryList.contains(insert))
+                        {
+                           mEntryList.add(insert);
+                           flag = true;
+                           StaticEntryList.getInstance().setMap(data.toString(), urlShots.getKey());
+                        }
                      }
-
+                  }
                }
 
             }
-            Log.d("DEBUG", Integer.toString(mEntryList.size()));
+            if (flag)
+               mAdapter.notifyDataSetChanged();
 
          }
 
          @Override
-         public void onCancelled(DatabaseError databaseError) {
+         public void onCancelled(DatabaseError databaseError)
+         {
 
          }
       });
 
 
-
    }
 
-   private void loadLogInView() {
+   private void loadLogInView()
+   {
       Intent intent = new Intent(this, LogInActivity.class);
       intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
       intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -144,26 +163,41 @@ public class MainActivity extends AppCompatActivity{
    }
 
    @Override
-   public Object onRetainCustomNonConfigurationInstance() {
+   public Object onRetainCustomNonConfigurationInstance()
+   {
       return mEntryList;
    }
 
    @Override
-   protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+   protected void onActivityResult(int requestCode, int resultCode, Intent data)
+   {
       super.onActivityResult(requestCode, resultCode, data);
-      if (requestCode == TEST_UPLOAD_INTENT && resultCode == RESULT_OK) {
+      if (requestCode == TEST_UPLOAD_INTENT && resultCode == RESULT_OK)
+      {
          Uri uri = data.getData();
-         StorageReference filePath = mStorage.getReference().child("Photos").child(mFirebaseUser.getUid()).child(Long.toString(System.currentTimeMillis()));
-         filePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+         StorageReference filePath = mStorage.getReference().child("Photos").child(mFirebaseUser
+               .getUid()).child(Long.toString(System.currentTimeMillis()));
+         filePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>()
+         {
             @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot)
+            {
                Toast.makeText(MainActivity.this, "Upload Done", Toast.LENGTH_LONG).show();
                Uri downloadUri = taskSnapshot.getDownloadUrl();
-               DatabaseReference newEntry = mDataBase.getInstance().getReference("users").child(mFirebaseUser.getUid()).push();
-               newEntry.child("url").setValue(downloadUri.toString());
-
+               storeImageToUser(downloadUri);
             }
          });
       }
+   }
+
+   private void storeImageToUser(Uri uri)
+   {
+      String userId = mFirebaseUser.getUid();
+      DatabaseReference newEntry = mDataBase.getInstance().getReference("users").child(userId);
+      DatabaseReference newImage = mDataBase.getInstance().getReference("images").push();
+      String imageKey = newImage.getKey();
+      newImage.child("url").setValue(uri.toString());
+      newEntry.child("Images").child(imageKey).setValue(uri.toString());
+
    }
 }
